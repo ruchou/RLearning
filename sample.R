01aff8cb6292c7abac81e540eb3f90bfbb8643b5
yanrujhou <joe04024@gmail.com>
2020-04-03 15:01:22 +0800

    add first files

---
+++ sample.R
@@ -0,0 +1,16 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: ruchou
+# Created on: 4/3/20
+
+#Tutorial from
+# https://medium.com/datainpoint/r-essentials-quick-start-8226b33514f9
+x <- 3
+x
+
+z <- c(38,41,34)
+
+print(z)
+
+print("Taiwan Value")
+rm(x)
